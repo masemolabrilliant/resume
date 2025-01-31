e49205bfcf46418dcb9343d6566fa57a7853b9f6
Mogaladi Brilliant <masemola.m98@gmail.com>
2025-01-31 20:45:46 +0200

    fixed hozional sclide

--- js/script.js
+++ js/script.js
@@ -20,4 +20,36 @@ resumeBtns.forEach((btn, idx)=> {
     
 });
 
-const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right')
+const arrowRight = document.querySelector('.portfolio-box .navigation .arrow-right');
+const arrowLeft = document.querySelector('.portfolio-box .navigation .arrow-left');
+
+let index = 0;
+const activePortfolio = () => {
+    const imgSlide = document.querySelector('.portfolio-carousel .img-slide ');
+
+    imgSlide.style.transform = `translateX(calc(${index * -100}% - ${index * 2}rem ))`;
+}
+
+arrowRight.addEventListener('click', ()=>{
+    if(index <4){
+        index++;
+        arrowLeft.classList.remove('disabled');
+    }else{
+        index = 5;
+        arrowRight.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
+
+arrowLeft.addEventListener('click', ()=>{
+    if(index > 1){
+        index--;
+        arrowRight.classList.remove('disabled');
+    }else{
+        index = 0;
+        arrowLeft.classList.add('disabled');
+    }
+
+    activePortfolio();
+});
